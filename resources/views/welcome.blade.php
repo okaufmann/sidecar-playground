7a843e1e37e5ed065d345a2e0d0e63f41df9738b
Oliver Kaufmann <oliver.kaufmann@novu.ch>
2022-02-09 22:59:24 +0100

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
         </script>
 
     </head>
-    <body class="antialiased font-sans text-white">
+    <body class="antialiased font-sans text-white px-10">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             <div class="flex flex-col space-y-5">
                 <h1 class="text-3xl">Sidecar Examples</h1>
